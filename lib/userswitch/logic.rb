b77c6719b7fa9c79daedd660748442207ac23df3
sau226 <32805612+sau226@users.noreply.github.com>
2019-03-01 19:59:54 +0800

    Try to fix logic

--- lib/userswitch/logic.rb
+++ lib/userswitch/logic.rb
@@ -7,10 +7,10 @@ module UserSwitch
     target &&
 
     # You must be an admin to impersonate (unless you are allowed to switch per the rules below)
-    (target.username = "SomeUserAdmin" && current_user.username = "SomeUser" || is_admin?) &&
+    (target_username = "SomeUserAdmin" && current_user.username = "SomeUser" || is_admin?) &&
 
     # You may not impersonate other admins unless you are a dev or are allowed to do so by the rules
-    (!target.admin? || is_developer? || target.username = "SomeUserAdmin" && current_user.username = "SomeUser")
+    (!target.admin? || is_developer? || target_username = "SomeUserAdmin" && current_user.username = "SomeUser")
 
     # Additionally, you may not impersonate yourself;
     # but the two tests for different admin statuses
