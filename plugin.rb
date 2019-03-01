6b7187a5c0bf74cfa65c749a7c45c1bf4ae39178
sau226 <32805612+sau226@users.noreply.github.com>
2019-03-01 20:13:40 +0800

    Move logic into main plugin file

--- plugin.rb
+++ plugin.rb
@@ -4,4 +4,26 @@
 # authors: sau226
 
 require 'guardian'
-load File.expand_path('../lib/userswitch/logic.rb', __FILE__)
+module UserSwitch
+  def self.included base
+    base.class_eval do
+
+     # Can we impersonate this user?
+     def can_impersonate?(target)
+       target &&
+
+       # You must be an admin to impersonate (unless you are allowed to switch per the rules below)
+       (target_username = "SomeUserAdmin" && current_user.username = "SomeUser" || is_admin?) &&
+
+       # You may not impersonate other admins unless you are a dev or are allowed to do so by the rules
+       (!target.admin? || is_developer? || target_username = "SomeUserAdmin" && current_user.username = "SomeUser")
+
+       # Additionally, you may not impersonate yourself;
+       # but the two tests for different admin statuses
+       # make it impossible to be the same user.
+     end
+   end
+ end
+end
+
+Guardian.send(:include, UserSwitch)
